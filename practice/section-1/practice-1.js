eee2e89108b3065211fdd2e574e3fdb6fe78d9ba
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 11:29:04 +0800

    update

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,18 +1,23 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  let elements = [];
-  collectionA.forEach(function(elementInA) {
-    // collectionB.find(elementInA);
-    // 箭头函数与匿名函数?
-    if (collectionB.includes(elementInA)) {
-      elements.push(elementInA);
+  // let elements = [];
+  // collectionA.forEach(function(elementInA) {
+  //   if (collectionB.includes(elementInA)) {
+  //     elements.push(elementInA);
+  //   }
+  //   // collectionB.find((elementInB) => {
+  //   //   elementInB === elementInA;
+  //   // })
+  // }, this);
+  // // es6
+  // // collectionA.forEach((v) => collectionB.includes(v) && elements.push(v));
+  // return elements;
+
+  return collectionA.map(v => {
+    if (collectionB.includes(v)) {
+      return v;
     }
-    // collectionB.find((elementInB) => {
-    //   elementInB === elementInA;
-    // })
-  }, this);
-  // es6
-  // collectionA.forEach((v) => collectionB.includes(v) && elements.push(v));
-  return elements;
+  })
+
 }
