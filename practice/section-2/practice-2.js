9cc175700967630baa65722e96e2fc037191553d
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 17:11:55 +0800

    fixed se-2 pra-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let counters = [];
+  for (let element of collection) {
+    let [key, count] = element.split('-');
+    let existedCounter = findObj(key, counters);
+    count = getCount(count);
+    if (existedCounter) {
+      existedCounter.count += count;
+    } else {
+      counters.push({key: key, count: count});
+    }
+  }
+  return counters;
+}
+
+function findObj(key, counters) {
+  for (let counter of counters) {
+    if (counter.key === key) {
+      return counter;
+    }
+  }
+}
+
+function getCount(count) {
+  if (count) {
+    return parseInt(count);
+  }
+  return 1;
 }
