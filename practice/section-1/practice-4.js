3987f34ea1a7e91e6ef0ca64d44e791fda43cda6
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 10:34:59 +0800

    fixed se-1 pra-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let elements = [];
+  collectionA.forEach(item => objectB.value.includes(item.key) && elements.push(item.key));
+  return elements;
 }
