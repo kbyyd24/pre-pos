fb8c5503810fba4f9dd45ff3f56c9e83ced48110
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 10:25:07 +0800

    update

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -3,11 +3,16 @@
 function collectSameElements(collectionA, collectionB) {
   let elements = [];
   collectionA.forEach(function(elementInA) {
-    collectionB.forEach(function(elementInB) {
-      if (elementInB === elementInA) {
-        elements.push(elementInA);
-      }
-    })
+    // collectionB.find(elementInA);
+    // 箭头函数与匿名函数?
+    if (collectionB.includes(elementInA)) {
+      elements.push(elementInA);
+    }
+    // collectionB.find((elementInB) => {
+    //   elementInB === elementInA;
+    // })
   }, this);
+  // es6
+  // collectionA.forEach((v) => collectionB.includes(v) && elements.push(v));
   return elements;
 }
