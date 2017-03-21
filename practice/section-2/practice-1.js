41f2cb22ed3622d1da949afd1e55bcf47a500995
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 17:11:42 +0800

    update

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,21 +1,49 @@
 'use strict';
 
 function countSameElements(collection) {
-  let counts = {};
-  collection.forEach(v => {
-    if (counts[v]) {
-      counts[v] += 1;
-    } else {
-      counts[v] = 1;
-    }
-  });
+  // let counts = {};
+  // collection.forEach(v => {
+  //   if (counts[v]) {
+  //     counts[v] += 1;
+  //   } else {
+  //     counts[v] = 1;
+  //   }
+  // });
+  // let answer = [];
+  // let keys = Object.keys(counts);
+  // keys.forEach(key => {
+  //   answer.push({
+  //     key: key,
+  //     count: counts[key]
+  //   });
+  // });
+  // return answer;
+
+
+  let mySet = new Set();
+  collection.forEach(element => mySet.add(element));
   let answer = [];
-  let keys = Object.keys(counts);
-  keys.forEach(key => {
+  mySet.forEach(element => {
     answer.push({
-      key: key,
-      count: counts[key]
+      key: element,
+      count: 0
     });
   });
+  for (let i = 0; i < answer.length; i++) {
+    let count = 0;
+    collection.forEach(element => {
+      if (element === answer[i].key) {
+        count += 1;
+      }
+    });
+    answer[i].count = count;
+  }
   return answer;
+
+  // return [... new Set(collection)].map(element => {
+  //   return {
+  //     key: element,
+  //     count: collection.filter(v => v === element).length
+  //   }
+  // })
 }
