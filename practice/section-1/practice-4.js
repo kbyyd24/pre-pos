aef737d57045ec818a71f49285f13f523c8d0364
gaoyx <melo@gaoyuexiang.cn>
2017-03-21 11:51:14 +0800

    update

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
+  // return collectionA
+  //   .filter(item => objectB.value.includes(item.key))
+  //   .map(item => item.key)
+
   return collectionA
-    .filter(item => objectB.value.includes(item.key))
     .map(item => item.key)
+    .filter(v => objectB.value.includes(v))
 }
